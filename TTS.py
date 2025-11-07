c9d14c284fa054497165b2fc4eec9ae298e0b6c1
Jahn, Karl Richard - SAXOPRINT <karlrichard.jahn@saxoprint.de>
2025-11-07 08:58:58 +0100

    languages!!!!

---
+++ TTS.py
@@ -0,0 +1,34 @@
+from typing import Literal
+import pyttsx3
+
+class TTS:
+
+    _engine = pyttsx3.init()
+    _language_codes = {
+        voice.languages[0].lower() : voice.id for voice in _engine.getProperty('voices')
+    }
+    
+    @staticmethod
+    def speak(text: str, language: str) -> None: #Literal["en", "de"] = "en") -> None:
+
+        print(TTS._language_codes)
+
+        language_code = TTS._language_codes.get(language, "en")
+        if not language_code: raise ValueError(f"Language '{language}' not supported.")
+
+        TTS.stop()
+
+        TTS._engine.setProperty('voice', language_code)
+        TTS._engine.say(text)
+        TTS._engine.startLoop()
+
+        TTS.stop()
+
+    @staticmethod
+    def stop() -> None:
+        try:
+            TTS._engine.endLoop()
+            TTS._engine.stop()
+        except Exception as e:
+            print(f"Error stopping TTS engine: {e}")
+        
--- main.py
+++ main.py
@@ -4,26 +4,30 @@ from typing import Literal
 
 
 #from PyQt6.QtCore import pyqtSlot
-from PySide6.QtUiTools import loadUiType
-from PySide6.QtCore import QCoreApplication, Signal, Slot, Qt
-from PySide6.QtWidgets import QApplication, QFileDialog
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import QCoreApplication, Signal, Slot, Qt, QFile, QObject
+from PySide6.QtWidgets import QApplication, QFileDialog, QSpinBox, QLabel, QWidget, QMainWindow
+#from PyQt6.uic import loadUi
 from qt_material import apply_stylesheet
 
-import DataManager
+import DataManager, TTS
 
-#GUI_FILENAME = "Frontend.ui"
-DE_UI_FILENAME = "de_DE.ui"
-EN_UI_FILENAME = "en_EN.ui"
-GUI_FILENAME = "de_DE.ui"
-LANGUAGE: Literal["DE", "EN"] = "DE"
-Form, Base = loadUiType(os.path.join(pathlib.Path(__file__).parent.resolve(), GUI_FILENAME)) # type: ignore
+GUI_FILENAMES = {"de": "de_DE.ui", "en": "en_EN.ui"}
+LANGUAGE: Literal["de", "en"] = "de"
+GUI_FILENAME = GUI_FILENAMES[LANGUAGE]
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 app = None
 
 
-class MainWindow(Base, Form):
+class MainWindow(QMainWindow):
     def __init__(self, parent=None):
-        super(self.__class__, self).__init__(parent)
-        self.setupUi(self)
+        #super(self.__class__, self).__init__(parent)
+        super(MainWindow, self).__init__()
+        self.loader = QUiLoader()
+        print(os.path.join(SCRIPT_DIR, GUI_FILENAME))
+        #loadUi(os.path.join(SCRIPT_DIR, GUI_FILENAME), self)
+        self.ui = self.loader.load(os.path.join(SCRIPT_DIR, GUI_FILENAME), None)
+        self.ui.show()
         self.connect_slots()
         self.dataManager = DataManager.DataManager(0,0,0,0,0,0,0, None)
         self.dataManager.calculate_all_grades()
@@ -32,57 +36,57 @@ class MainWindow(Base, Form):
 
     def load_ui_values_to_data_manager(self):
         self.dataManager.set_all_values(
-            fe1_ItWorkstation=self.numFe1.value(),
-            fe2_PlanningASoftwareProduct=self.numFe2_1.value(),
-            fe2_DevelopmentAndImplementationOfAlgorithms=self.numFe2_2.value(),
-            fe2_EconomicsAndSocialStudies=self.numFe2_3.value(),
-            fe2_PlanningAndImplementingASoftwareProduct_Oral=self.numFe2_8.value(),
-            fe2_PlanningAndImplementingASoftwareProduct_Written=self.numFe2_7.value(),
+            fe1_ItWorkstation=self.ui.numFe1.value(),
+            fe2_PlanningASoftwareProduct=self.ui.numFe2_1.value(),
+            fe2_DevelopmentAndImplementationOfAlgorithms=self.ui.numFe2_2.value(),
+            fe2_EconomicsAndSocialStudies=self.ui.numFe2_3.value(),
+            fe2_PlanningAndImplementingASoftwareProduct_Oral=self.ui.numFe2_8.value(),
+            fe2_PlanningAndImplementingASoftwareProduct_Written=self.ui.numFe2_7.value(),
             fe2_OralSupplementaryExamination=None,
             fe2_OralSupplementaryExaminationSubject=None
         )
 
 
     def set_ui_values_by_data_manager(self):
-        self.numFe1.setValue(self.dataManager.fe1_ItWorkstation)
-        self.numFe2_1.setValue(self.dataManager.fe2_PlanningASoftwareProduct)
-        self.numFe2_2.setValue(self.dataManager.fe2_DevelopmentAndImplementationOfAlgorithms)
-        self.numFe2_3.setValue(self.dataManager.fe2_EconomicsAndSocialStudies)
-        self.numFe2_7.setValue(self.dataManager.fe2_PlanningAndImplementingASoftwareProduct_Written)
-        self.numFe2_8.setValue(self.dataManager.fe2_PlanningAndImplementingASoftwareProduct_Oral)
-        self.numFe2_out.setValue(self.dataManager.fe2_PlanningAndImplementingASoftwareProduct_Score)
+        self.ui.numFe1.setValue(self.dataManager.fe1_ItWorkstation)
+        self.ui.numFe2_1.setValue(self.dataManager.fe2_PlanningASoftwareProduct)
+        self.ui.numFe2_2.setValue(self.dataManager.fe2_DevelopmentAndImplementationOfAlgorithms)
+        self.ui.numFe2_3.setValue(self.dataManager.fe2_EconomicsAndSocialStudies)
+        self.ui.numFe2_7.setValue(self.dataManager.fe2_PlanningAndImplementingASoftwareProduct_Written)
+        self.ui.numFe2_8.setValue(self.dataManager.fe2_PlanningAndImplementingASoftwareProduct_Oral)
+        self.ui.numFe2_out.setValue(self.dataManager.fe2_PlanningAndImplementingASoftwareProduct_Score)
 
         if self.dataManager.fe1_ItWorkstation_Grade:
-            self.lblFe1Output.setText(str(self.dataManager.fe1_ItWorkstation_Grade))
+            self.ui.lblFe1Output.setText(str(self.dataManager.fe1_ItWorkstation_Grade))
         if self.dataManager.fe2_PlanningASoftwareProduct_Grade:
-            self.lblFe2Output1.setText(str(self.dataManager.fe2_PlanningASoftwareProduct_Grade))
+            self.ui.lblFe2Output1.setText(str(self.dataManager.fe2_PlanningASoftwareProduct_Grade))
         if self.dataManager.fe2_DevelopmentAndImplementationOfAlgorithms_Grade:
-            self.lblFe2Output2.setText(str(self.dataManager.fe2_DevelopmentAndImplementationOfAlgorithms_Grade))
+            self.ui.lblFe2Output2.setText(str(self.dataManager.fe2_DevelopmentAndImplementationOfAlgorithms_Grade))
         if self.dataManager.fe2_EconomicsAndSocialStudies_Grade:
-            self.lblFe2Output3.setText(str(self.dataManager.fe2_EconomicsAndSocialStudies_Grade))
+            self.ui.lblFe2Output3.setText(str(self.dataManager.fe2_EconomicsAndSocialStudies_Grade))
         if self.dataManager.fe2_PlanningAndImplementingASoftwareProduct_Grade:
-            self.lblFe2Output1_3.setText(str(self.dataManager.fe2_PlanningAndImplementingASoftwareProduct_Grade))
+            self.ui.lblFe2Output1_3.setText(str(self.dataManager.fe2_PlanningAndImplementingASoftwareProduct_Grade))
 
         self.set_success()
 
 
     def set_success(self):
-        self.lblSuccess.setText("Die Prüfung wurde bestanden." if self.dataManager.has_passed() else "Die Prüfung wurde nicht bestanden.")
+        self.ui.lblSuccess.setText("Die Prüfung wurde bestanden." if self.dataManager.has_passed() else "Die Prüfung wurde nicht bestanden.")
 
-
+    @Slot()
     def on_numValue_changed(self):
         self.load_ui_values_to_data_manager()
         self.set_ui_values_by_data_manager()
 
-
+    @Slot()
     def set_dark_mode(self):
         apply_stylesheet(app, "dark_teal.xml")
 
-
+    @Slot()
     def set_bright_mode(self):
         apply_stylesheet(app, "light_blue.xml")
 
-
+    @Slot()
     def load_from_file_action(self):
         fileName, _ = QFileDialog.getOpenFileName(self, 
                                                   caption="Eingabeparameter aus Datei laden",
@@ -94,7 +98,7 @@ class MainWindow(Base, Form):
             self.set_ui_values_by_data_manager()
             self._connect_number_slots()
 
-
+    @Slot()
     def save_to_file_action(self):
         fileName, _ = QFileDialog.getSaveFileName(self,
                                                   caption="Eingabeparameter in Datei speichern",
@@ -102,31 +106,53 @@ class MainWindow(Base, Form):
         if fileName:
             self.dataManager.saveToFile(fileName)
 
+    def say(self):
+        TTS.TTS.speak("Hello, this is a test.", language="fr-fr")
+        TTS.TTS.speak("Hallo, dies ist ein Test.", language="de-de")
+
+    def change_ui_file(self, language: Literal["de", "en"]):
+        self.disconnect_slots()
+        LANGUAGE = language
+        #self.ui.close()
+        self.ui = self.loader.load(os.path.join(SCRIPT_DIR, GUI_FILENAMES[LANGUAGE]), None)
+        self.ui.show()
+        self.connect_slots()
+        self.set_ui_values_by_data_manager()
 
     def connect_slots(self):
         self._connect_number_slots()
-        self.actionBright.triggered.connect(self.set_bright_mode)
-        self.actionDark.triggered.connect(self.set_dark_mode)
-        self.actionLoad.triggered.connect(self.load_from_file_action)
-        self.actionSave.triggered.connect(self.save_to_file_action)
+        self.ui.actionBright.triggered.connect(self.set_bright_mode)
+        self.ui.actionDark.triggered.connect(self.set_dark_mode)
+        self.ui.actionLoad.triggered.connect(self.load_from_file_action)
+        self.ui.actionSave.triggered.connect(self.save_to_file_action)
+        self.ui.actionEnglish.triggered.connect(lambda: self.change_ui_file("en"))
+        self.ui.actionGerman.triggered.connect(lambda: self.change_ui_file("de"))
 
+    def disconnect_slots(self):
+        self._disconnect_number_slots()
+        self.ui.actionBright.triggered.disconnect()
+        self.ui.actionDark.triggered.disconnect()
+        self.ui.actionLoad.triggered.disconnect()
+        self.ui.actionSave.triggered.disconnect()
+        self.ui.actionEnglish.triggered.disconnect()
 
     def _connect_number_slots(self):
-        self.numFe1.valueChanged.connect(lambda num: self.on_numValue_changed())
-        self.numFe2_1.valueChanged.connect(lambda num: self.on_numValue_changed())
-        self.numFe2_2.valueChanged.connect(lambda num: self.on_numValue_changed())
-        self.numFe2_3.valueChanged.connect(lambda num: self.on_numValue_changed())
-        self.numFe2_7.valueChanged.connect(lambda num: self.on_numValue_changed())
-        self.numFe2_8.valueChanged.connect(lambda num: self.on_numValue_changed())
+        self.ui.numFe1.valueChanged.connect(lambda num: self.on_numValue_changed())
+        self.ui.numFe1.valueChanged.connect(lambda num: self.say()) 
+        self.ui.numFe2_1.valueChanged.connect(lambda num: self.on_numValue_changed())
+        self.ui.numFe2_2.valueChanged.connect(lambda num: self.on_numValue_changed())
+        self.ui.numFe2_3.valueChanged.connect(lambda num: self.on_numValue_changed())
+        self.ui.numFe2_7.valueChanged.connect(lambda num: self.on_numValue_changed())
+        self.ui.numFe2_8.valueChanged.connect(lambda num: self.on_numValue_changed())
 
 
     def _disconnect_number_slots(self):
-        self.numFe1.valueChanged.disconnect()
-        self.numFe2_1.valueChanged.disconnect()
-        self.numFe2_2.valueChanged.disconnect()
-        self.numFe2_3.valueChanged.disconnect()
-        self.numFe2_7.valueChanged.disconnect()
-        self.numFe2_8.valueChanged.disconnect()
+        self.ui.numFe1.valueChanged.disconnect()
+        self.ui.numFe2_1.valueChanged.disconnect()
+        self.ui.numFe2_2.valueChanged.disconnect()
+        self.ui.numFe2_3.valueChanged.disconnect()
+        self.ui.numFe2_7.valueChanged.disconnect()
+        self.ui.numFe2_8.valueChanged.disconnect()
 
 
 
@@ -135,13 +161,13 @@ def main():
     QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
     app = QApplication(sys.argv)
     #os.chdir(sys.path[1])
-    widget = MainWindow()
+    window = MainWindow()
 
     # from qt_material
     #apply_stylesheet(app, "dark_teal.xml")
     apply_stylesheet(app, "light_blue.xml")
 
-    widget.show()
+    #window.show()
     sys.exit(app.exec())
 
 if __name__ == "__main__":
